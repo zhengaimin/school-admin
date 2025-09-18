e0a4ad9a0da2bc73098fe3223942c7ba96d2aa93
Kyle <zhangkui@geniuscarbon.cn>
2025-09-18 16:38:12 +0800

    fix typo

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ssh xk "rm -fr services/app/dist"
-rsync -avzP dist services/app/dist
+ssh xk "rm -fr /home/app/services/app/dist"
+rsync -avzP dist xk:/home/app/services/app/
 
 ssh xk docker service update --container-label-add last_deployed=$(date -u +%Y-%m-%dT%H:%M:%S) app_app
